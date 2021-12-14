9d41a81669da725c0332e8ba5cdda9cb6961e168
farrukh90 <farrukhsadykov@gmail.com>
2021-12-14 00:19:23 -0600

    added readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # terraform-digitalocean-droplet
 ```
 module "swarm" {
-  source   = "../"
+  source   = "farrukh90/droplet/digitalocean"
   do_token = "YOUR_TOKEN"
   ssh_keys = ["FINGERPRINT"]
 }
--- examples/module.tf
+++ examples/module.tf
@@ -1,5 +1,5 @@
 module "swarm" {
-  source   = "../"
+  source   = "farrukh90/droplet/digitalocean"
   do_token = "d81dcf990f3fb04432d545b2ce32c5ffe28313f7f13971677aeeb41598677d87"
   ssh_keys = ["e7:68:15:7b:73:24:3b:ac:1f:8f:90:89:8a:88:bf:41"]
 }
