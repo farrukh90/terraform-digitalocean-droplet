4349cddf1a453f5f3a232c1ffde19b96c38c8d2d
Emir Koroglu <69024539+emirsway@users.noreply.github.com>
2021-12-14 18:48:23 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
 variable "do_token" {
-   default ="b8f4298a8edf0bb82318308ad974d160f89a068fc2a30cc72cab4365af91ece3"
+   default ="Add your api token here"
 }
 
 #Put your remote machine ssh publickey fingerprint to below after you put it to digital ocean.
 variable "ssh_keys" {
   type = list(any)
   default = [
-    "a7:02:75:55:06:95:20:96:57:38:e3:49:a4:06:55:3b"
+    "Add your ssh publickey fingerprint here"
   ]
 }
